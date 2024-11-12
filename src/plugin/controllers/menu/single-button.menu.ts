4209649927ead1e5cfe29d6402ccc9d5067bb371
dt_0434724566 <1316346949@qq.com>
2024-11-12 17:51:36 +0800

    自定义右键菜单栏图标功能1

--- src/plugin/controllers/menu/single-button.menu.ts
+++ src/plugin/controllers/menu/single-button.menu.ts
@@ -11,7 +11,7 @@ export function CustomMenuItemSingleButtonFactory(): IMenuButtonItem<string> {
         // The type of the menu item, in this case, it is a button
         type: MenuItemType.BUTTON,
         // The icon of the button, which needs to be registered in ComponentManager
-        icon: 'ButtonIcon',
+        icon: 'ButtonIcon',//此处是右键自定义菜单的icon
         // The tooltip of the button. Prioritize matching internationalization. If no match is found, the original string will be displayed
         tooltip: 'customMenu.singleButton',
         // The title of the button. Prioritize matching internationalization. If no match is found, the original string will be displayed
